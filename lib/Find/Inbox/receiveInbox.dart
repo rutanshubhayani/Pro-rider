1ca738c4e086c9afbbceb2d5a79933bcbe6160ea
rutanshubhayani <125697596+rutanshubhayani@users.noreply.github.com>
2024-12-04 10:56:03 +0530

    04-12-2024

--- lib/Find/Inbox/receiveInbox.dart
+++ lib/Find/Inbox/receiveInbox.dart
@@ -430,7 +430,6 @@ class _InboxListState extends State<InboxList> with TickerProviderStateMixin {
                     onSubmitted: (_) {
                       FocusScope.of(context).unfocus();
                     },
-
                     decoration: InputDecoration(
                       prefixIcon: const Icon(Icons.search),
                       hintText: 'Search',
